95f32600c11c95c8d6549e9685c557561d3b51ae
Florian Scholz <Florian Scholz>
2017-04-09 18:39:07 +0200

    Projektdateien hinzufügen.

---
+++ InformationRetrieval/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InformationRetrieval
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

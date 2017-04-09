079a5aba2f704b1fd836c013d14e4d6fb34c3fc9
Florian Scholz <florian.scholz@stud.hn.de>
2017-04-09 23:03:01 +0200

    Register Modules to DI

--- InformationRetrieval/Program.cs
+++ InformationRetrieval/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using InformationRetrieval.ExpressionParser;
+using InformationRetrieval.Index;
+using InformationRetrieval.Tokenizer;
 using SimpleInjector;
 
 namespace InformationRetrieval
@@ -8,7 +11,13 @@ namespace InformationRetrieval
         static void Main(string[] args)
         {
             Container diContainer = new Container();
-            
+            diContainer.RegisterSingleton<IIndex, Index.Index>();
+            diContainer.RegisterSingleton<IExpressionParser,ExpressionParser.ExpressionParser>();
+            diContainer.RegisterSingleton<ITokenizer, Tokenizer.Tokenizer>();
+            diContainer.Verify();
+
+            IIndex index = diContainer.GetInstance<IIndex>();
+
         }
     }
 }

5af2356ba02b1f61a5ad23fd203e788c7a514671
Florian Scholz <florian90@gmail.com>
2017-04-27 14:52:14 +0200

    Initial implementation for Fuzzy logics

--- InformationRetrieval/Index/Index.cs
+++ InformationRetrieval/Index/Index.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections.Generic;
 using System.Collections.Immutable;
 using InformationRetrieval.Tokenizer;
+using MathNet.Numerics.LinearAlgebra.Double;
 
 namespace InformationRetrieval.Index
 {
     public class Index : IIndex
     {
+        private SparseMatrix sparseMatrix = new SparseMatrix(0, 0);
         public SortedList<string, Term> Terms { get;  } = new SortedList<string, Term>();
         public SortedSet<Posting> AllDocuments { get; set; } = new SortedSet<Posting>();
         public void InsertPostings(List<Token> tokens, string filename)
@@ -62,6 +64,24 @@ namespace InformationRetrieval.Index
         public SortedSet<Posting> GetAllDocuments()
         {
             return new SortedSet<Posting>(AllDocuments);
+        }
+
+        /// <summary>
+        /// Calculates Jaccard
+        /// </summary>
+        public void CalculateJaccard()
+        {
+            this.sparseMatrix = new SparseMatrix(this.AllDocuments.Count, Terms.Count);
+            
+            /* Documents */
+            for (int i = 0; i < sparseMatrix.RowCount; i++)
+            {
+                /* Terms */
+                for (int j = 0; j < sparseMatrix.ColumnCount; j++)
+                {
+                    sparseMatrix[i, j] = 1.0;
+                }
+            }
         }
 
         public bool GetPosting(string token, out Term term)
